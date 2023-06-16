30a1936cd12a243fe623bea85acad55680842605
ThatFireBoi <gcf2007@hotmail.com>
2023-06-16 13:44:43 +0000

    C program that prints an exact string using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string in the printf function
+ *
+ * Description: Function prints out "with proper grammar,
+ * but the outcome is a piece of art
+ *
+ * Return: value of void is zero.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
