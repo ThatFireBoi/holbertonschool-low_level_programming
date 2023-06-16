3c32936a147a45142e33a30697d83b860d87f1cd
ThatFireBoi <gcf2007@hotmail.com>
2023-06-16 17:47:11 +0000

    C program that prints the size of various types on the computer it is compiled and run on

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - With the provided variables print the strings provided
+ * main - print the strings in the printf functions
  *
  * Description: using the main function, print the size
  * of the various types available on the computer.
  *
  * Return: a value of 0
  */
-
 int main(void)
 {
 	char c;
