af4e4e7825bf9099862f5fbe2cfe724c8df3b754
ThatFireBoi <gcf2007@hotmail.com>
2023-06-16 13:23:33 +0000

    C program that prints an exact string using puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ *Main - print the provided string by using puts.
+ *
+ * Puts : prints out Programming is like building a multilingual puzzle.
+ *
+ * Returns 0.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
