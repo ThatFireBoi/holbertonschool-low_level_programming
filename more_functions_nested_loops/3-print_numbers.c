9e3313e6a48b6ce3495d97a512128993de2aace5
ThatFireBoi <gcf2007@hotmail.com>
2023-06-23 18:32:25 +0000

    Function that prints the numbers, from 0 to 9, followed by a new line

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point, prints numbers from 0 to 9
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+}
