37922b2b94574461633e02da8847feef4d346ff4
ThatFireBoi <gcf2007@hotmail.com>
2023-07-14 19:34:34 +0000

    Prints the name of the file it was compiled from, followed by a new line v.3

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/* main: prints the name of the file it was compiled from.
+/**
+ * main - prints the name of the file it was compiled from.
  *
  * return: zero
- *
  */
 
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 
-	return 0;
+	return (0);
 }
