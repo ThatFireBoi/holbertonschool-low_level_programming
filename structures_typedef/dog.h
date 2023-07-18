7d38f066e3bc2d0ea9a93dd261ea7200d972e4e3
ThatFireBoi <gcf2007@hotmail.com>
2023-07-18 20:45:12 +0000

    Defines a new type dog_t as a new name for the type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -19,6 +19,8 @@ char *name;
 float age;
 char *owner;
 };
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
