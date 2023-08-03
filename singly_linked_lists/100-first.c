5c0b8353a01ac2f93135f4b63fc302b541a2af6c
ThatFireBoi <gcf2007@hotmail.com>
2023-08-03 14:47:39 +0000

    Function that prints line before main is executed v.2

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-void __atribute__((constructor)) theHare(void);
-
 /**
- * theHare - string printed before the main function is executed
+ * the_hare - string printed before the main function is executed
  *
  */
 
-void theHare(void)
+void __attribute__ ((constructor)) the_hare()
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
