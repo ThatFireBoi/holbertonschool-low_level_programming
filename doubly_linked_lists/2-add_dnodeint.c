fdefcbd4fd8b77d9a619d4cc9511dfa3c94600b2
ThatFireBoi <gcf2007@hotmail.com>
2023-08-04 22:15:50 +0000

    Function that adds a new mode at the beginning of a dlistint_t list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * add_dnodeint - Function that adds a new node at the beginning of a list
+ *
+ * @head: A pointer to the head of the list
+ *
+ * @str: integer for the new node
+ *
+ * Return: the address of the new element; NULL if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+
+	return (new);
+}
